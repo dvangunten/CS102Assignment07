f094b5d767845b5265c79550b16553c41b8209de
dvangunten <112978146+dvangunten@users.noreply.github.com>
2022-11-10 21:07:18 -0600

    Add files via upload

---
+++ MainClassHanoi.java
@@ -0,0 +1,42 @@
+package towersofhanoi4;
+
+public class MainClassHanoi 
+{
+
+    
+   public static void main(String[] args)
+    {
+        towerOfHanoi(4, "A", "B", "C", "D");
+        
+        
+        
+    }
+    static void towerOfHanoi(int numOfDisks, String from, String to, String helper, String helper2)
+    {
+        
+        
+        
+        if(numOfDisks == 0)
+        {
+           
+            return;
+            
+        }
+        if(numOfDisks == 1)
+        {
+            System.out.println("Move disk " + numOfDisks + " from rod " + from + " to rod " + to);
+            
+            
+            return;
+        }
+        
+        towerOfHanoi(numOfDisks - 2, from, helper, to, helper2 );
+        System.out.println("Move disk " + (numOfDisks - 1) + " from rod " + from + " to rod " + helper2);
+        System.out.println("Move disk " + numOfDisks + " from rod " + from + " to rod " + to);
+        System.out.println("Move disk " + (numOfDisks - 1) + " from rod " + helper2 + " to rod " + to);
+        towerOfHanoi(numOfDisks - 2, from, helper, to, helper2); 
+        
+        
+    }
+    
+}
